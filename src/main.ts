cf0a37e4be3be2105c5c2367684e944f96f52f51
Farabi <programmer.farabi.224@gmail.com>
2023-05-11 20:28:16 +0600

    feat: add faker user generator rout including it's features

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 import express from "express";
 import randomNumberController from "./controllers/randomNumberController.js";
+import userController from "./controllers/userController.js";
 
 const app = express();
 app.use(express.json());
@@ -9,5 +10,6 @@ dotenv.config();
 const PORT = process.env.PORT;
 
 app.get("/randomNumber", randomNumberController);
+app.post("/fakeUser", userController);
 
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
